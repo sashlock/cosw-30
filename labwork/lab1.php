915ac8fc2fd0b5419898a56e3ed97195481b318f
EC2 Default User <ec2-user@ip-172-31-84-75.ec2.internal>
2019-08-29 03:14:56 +0000

    My first commit

---
+++ labwork/lab1.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head><title>Lab Assignment no. 1</title></head>
+    <body>
+        <h1>Scott Ashlock</h1>
+            <p>Cackle fruit yo-ho-ho Nelsons folly case shot starboard walk the plank reef sails 
+            black jack Pirate Round red ensign loaded to the gunwalls parrel line matey clap of 
+            thunder fire ship crack Jennys tea cup keelhaul boatswain nipper pirate broadside bounty 
+            lee dead men tell no tales Sea Legs aye Brethren of the Coast tender topgallant barkadeer 
+            hempen halter run a rig rum sheet hogshead fire in the hole long clothes rope's end driver 
+            clipper hang the jib salmagundi spanker gun pressgang yardarm run a shot across the 
+            bow spirits Spanish Main.</p>
+            
+            
+            
+            <?php
+            echo 'This is a PHP paragraph.';
+            ?>
+    </body>
+</html>
