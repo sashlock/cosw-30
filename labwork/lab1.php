a37af2bb7231bfb7aebb928187f8f970461f13a4
EC2 Default User <ec2-user@ip-172-31-84-75.ec2.internal>
2019-09-01 20:38:18 +0000

    lab1.php update 003

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -17,18 +17,19 @@
             dabbled in web design here and there since back when GeoCities was still a thing. 
             I think I made my first website in 1996 or so and edited the code using WebTV since 
             I did not have computer at the time. Beyond web development and programming I spend 
-            most of my time listening to podcasts or reading. Some of my favorite podcasts are 
-            <ul>
-                <li><a href="https://www.dharmapunxnyc.com" target="_blank">Dharma Punx NYC</a>: Dharma Talks and Meditations by Josh Korda</li>
-                <li><a href="https://www.dolloppodcast.com" target="_blank">The Dollop</a></li>
-                <li><a href="https://runesoup.com/podcast" target="_blank">Rune Soup</a>: I consider myself to be a fairly skeptical person but this gives me a weekly dose of woo 🤣</li>
-            </ul>This is 
-            my <a href="https://github.com/sashlock" target="_blank">GitHub profile</a>.</p>
+            most of my time listening to podcasts or reading. </p>
             
             
             
             <?php
-            echo '<p>.</p>';
+            echo "<p>Here are a few of the podcast I enjoy:
+                    <ul>
+                        <li><a href='https://www.dharmapunxnyc.com/' target='_blank'>Dharma Punx NYC</a>: Dhama talks and mediations by Josh Korda</li>
+                        <li><a href='http://thedollop.libsyn.com/' target='_blank'>The Dollop</a></li>
+                        <li><a href='https://www.bbc.co.uk/programmes/b006qykl/episodes/downloads' target='_blank'>In Our Time</a></li>
+                    </ul>
+                    
+                </p>";
             ?>
     </body>
 </html>
