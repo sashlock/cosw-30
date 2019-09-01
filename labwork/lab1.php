bd34eedb3b184e831c14731d1d8513fa10e6b501
EC2 Default User <ec2-user@ip-172-31-84-75.ec2.internal>
2019-09-01 19:54:30 +0000

    updated lab1

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -1,20 +1,27 @@
 <!doctype html>
 <html>
-    <head><title>Lab Assignment no. 1</title></head>
+    <head>
+        <title>Labwork 1: Hello World</title>
+    </head>
     <body>
         <h1>Scott Ashlock</h1>
-            <p>Cackle fruit yo-ho-ho Nelsons folly case shot starboard walk the plank reef sails 
-            black jack Pirate Round red ensign loaded to the gunwalls parrel line matey clap of 
-            thunder fire ship crack Jennys tea cup keelhaul boatswain nipper pirate broadside bounty 
-            lee dead men tell no tales Sea Legs aye Brethren of the Coast tender topgallant barkadeer 
-            hempen halter run a rig rum sheet hogshead fire in the hole long clothes rope's end driver 
-            clipper hang the jib salmagundi spanker gun pressgang yardarm run a shot across the 
-            bow spirits Spanish Main.</p>
+            <p>Hello. My name is Scott and I am a web development student. I should have my
+            associate degree in web development by the end of the Fall 2019 semester. I've 
+            dabbled in web design here and there since back when GeoCities was still a thing. 
+            I think I made my first website in 1996 or so and edited the code using WebTV since 
+            I did not have computer at the time. Beyond web development and programming I spend 
+            most of my time listening to podcasts or reading. Some of my favorite podcasts are 
+            <ul>
+                <li><a href="https://www.dharmapunxnyc.com" target="_blank">Dharma Punx NYC</a>: Dharma Talks and Meditations by Josh Korda</li>
+                <li><a href="https://www.dolloppodcast.com" target="_blank">The Dollop</a></li>
+                <li><a href="https://runesoup.com/podcast" target="_blank">Rune Soup</a>: I consider myself to be a fairly skeptical person but this gives me a weekly dose of woo 🤣</li>
+            </ul>This is 
+            my <a href="https://github.com/sashlock" target="_blank">GitHub profile</a>.</p>
             
             
             
             <?php
-            echo 'This is a PHP paragraph.';
+            echo '<p>.</p>';
             ?>
     </body>
 </html>
