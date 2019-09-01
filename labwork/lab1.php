4f541aebcc918d83953c1d0f9066fd2285dff9bf
EC2 Default User <ec2-user@ip-172-31-84-75.ec2.internal>
2019-09-01 21:08:23 +0000

    lab1.php update 005

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -29,13 +29,17 @@
             
             
             <?php
-            echo "<p>Here are a few of the podcast I enjoy:
+            echo "<p>Here are a couple of the podcasts I enjoy:
                     <ul>
-                        <li><a href='https://www.dharmapunxnyc.com/' target='_blank'>Dharma Punx NYC</a>: Dhama talks and mediations by Josh Korda</li>
-                        <li><a href='http://thedollop.libsyn.com/' target='_blank'>The Dollop</a></li>
-                        <li><a href='https://www.bbc.co.uk/programmes/b006qykl/episodes/downloads' target='_blank'>In Our Time</a></li>
+                        <li><a href='https://www.dharmapunxnyc.com/' target='_blank'>Dharma Punx NYC</a>: Dharma talks and mediations by secular and unaffiliated Buddhist teacher Josh Korda</li>
+                        <li><a href='http://thedollop.libsyn.com/' target='_blank'>The Dollop</a>: This is a podcast is hosted by two commedians, Dave Anthony and Gereth Reynolds. They discuss
+                        a funny or odd story from American history.</li>
                     </ul>
-                    
+                    This assignment was fairly challenging due to having working with the command line. Remembering the exact order of commands to update and push this assignment to GitHub so 
+                    it would show up in Heroku was difficult at first. I think I have a a fairly good hang of it now.
+                    /* lab1.php
+                    Scott Ashlock
+                    09/01/19 */
                 </p>";
             ?>
     </body>
